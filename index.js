ca4f9c9017e75b5ea3bb001144cb999a57130f23
Gary <gp@limestripes.us>
2023-07-31 14:10:03 -0600

    move to github

---
+++ index.js
@@ -0,0 +1,11 @@
+var es_ES = require("./es.json");
+var zh_TW = require("./zh.json");
+var pt_BR = require("./pt.json");
+var fr_FR = require("./fr.json");
+var ru_RU = require("./ru.json");
+
+exports.es_ES = es_ES;
+exports.zh_TW = zh_TW;
+exports.pt_BR = pt_BR;
+exports.fr_FR = fr_FR;
+exports.ru_RU = ru_RU;
